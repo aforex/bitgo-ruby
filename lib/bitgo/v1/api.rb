0ef92b4edda1f475ffc2b44addc315a1791e278e
Unknown <zhdanovmvd@gmail.com>
2018-09-21 14:12:37 +0200

    Update sendcoins url

--- lib/bitgo/v1/api.rb
+++ lib/bitgo/v1/api.rb
@@ -246,7 +246,7 @@ module Bitgo
         call :post, '/wallet/' + wallet_id + '/address/' + chain
       end
 
-      def send_coins_to_address(wallet_id:, address:, amount:, walletPassphrase:, minConfirms: nil, fee: nil)
+      def send_coins_to_address(wallet_id:, address:, amount:, currency:,walletPassphrase:, minConfirms: nil, fee: nil)
         #NOTE: pass thru `fee` and `min_confirmations` only if they're present
         params = {
           address: address,
@@ -256,7 +256,7 @@ module Bitgo
           fee: fee
         }.delete_if { |_, v| v.nil? }
 
-        call :post, '/wallet/' + wallet_id + '/sendcoins', params
+        call :post, currency + '/wallet/' + wallet_id + '/sendcoins', params
       end
 
       def send_coins_to_multiple_addresses()
